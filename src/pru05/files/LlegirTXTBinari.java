34cd9cabd07b6cf6beb212893d70f6abcf4431f8
Xavier <Xavier@192.168.2.119>
2019-01-25 15:14:22 +0100

    Exemples de fitxers

---
+++ src/pru05/files/LlegirTXTBinari.java
@@ -0,0 +1,29 @@
+package pru05.files;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LlegirTXTBinari {
+	public static final String NOM_FITXER="c:\\tmp\\parells.dat";
+
+	public static void main(String[] args) {
+		
+		int dadallegida;
+		UtilsProgramacio utils=new UtilsProgramacio();
+		try {
+			InputStream inputstream = new FileInputStream(NOM_FITXER);
+			dadallegida=inputstream.read();
+			while (dadallegida!=-1) {
+				System.out.println(utils.MostraBinari(dadallegida,8)+" - "+dadallegida+" - "+ (char)dadallegida);
+				dadallegida=inputstream.read();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No s'ha trobat l'arxiu");
+		} catch (IOException e) {
+			System.out.println("S'ha produit un error llegint el fitxer");
+		}
+	}
+
+}
---
+++ src/pru05/files/PRU05_exemple_lectura_bytes.java
@@ -0,0 +1,43 @@
+package pru05.files;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+/**
+ * @author Xavier
+ *
+ */
+public class PRU05_exemple_lectura_bytes {
+
+	/**
+	 * @param args
+	 */
+	public static final String NOM_FITXER="c:\\tmp\\parells.dat";
+	public static void main(String[] args) throws FileNotFoundException {
+		InputStream inputstream = new FileInputStream(NOM_FITXER);
+
+		int data;
+		try {
+			data = inputstream.read();
+			while(data != -1) {
+				//do something with data...
+				System.out.println("Raw data:"+data);
+				System.out.println("Interpreted data:"+Character.toString ((char) data));
+
+				data = inputstream.read();
+			}
+			inputstream.close();	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+}
