82a482a2c55ab28ecfe5882a87ee2d9afafe854b
Xavier <Xavier@192.168.2.119>
2019-02-05 20:59:59 +0100

    Pujats exemples connexió BD

---
+++ src/prU06/PRU06Test_Connexio.java
@@ -0,0 +1,10 @@
+package prU06;
+
+
+public class PRU06Test_Connexio {
+    public static void main(String[] args) throws Exception {
+    	PRU06Exemple_connexio accessabd = new PRU06Exemple_connexio();
+        accessabd.readDataBase();
+    }
+
+}
